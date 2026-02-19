9d4a76243e20af077879210a492fcfae775458a1
Sévrain Chea <sevrain.chea@gmail.com>
2026-02-19 13:55:23 +0100

    feat: add config set command to config command group

--- src/commands/config/index.ts
+++ src/commands/config/index.ts
@@ -1,8 +1,10 @@
 import { Command } from 'commander';
 import { configExportCommand } from './export.js';
+import { configSetCommand } from './set.js';
 
 export function configCommand(): Command {
   const cmd = new Command('config').description('Manage local config');
   cmd.addCommand(configExportCommand());
+  cmd.addCommand(configSetCommand());
   return cmd;
 }
